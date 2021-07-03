f2f177a62e48afadedf19566630fcd591965de3a
sunjh754 <52984559+sunjh754@users.noreply.github.com>
2021-07-03 15:14:11 +0800

    Update Error.vue

--- VuePage/ClassVue/src/components/Error.vue
+++ VuePage/ClassVue/src/components/Error.vue
@@ -11,7 +11,7 @@
             this.$alert('登录信息失效！', '提示', {
                 confirmButtonText: '确定'
             }).then((response) => {
-                localStorage.removeItem('access-admin')
+                localStorage.removeItem('User')
                 _this.$router.replace({path: '/'})
             }).catch(() => {
           			 _this.$router.replace({path: '/'})         
